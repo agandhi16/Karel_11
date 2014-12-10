5c09e3e527ec6563775a491dccd12a75dcd073c3
agandhi16 <alex_gandhi@asl.org>
2014-12-10 11:31:08 +0000

    Finished Code

--- Lab11.java
+++ Lab11.java
@@ -6,12 +6,28 @@
  *
  */
 
-import edu.fcps.karel2.Display; 
+import edu.fcps.karel2.Display;
+import edu.fcps.Digit; 
 
 public class Lab11 {
 	 
 	 public static void main(String[] args) {
-	
-	 }
-	 
+    
+    Display.openWorld("maps/.map");
+	   Display.setSize(36, 32);
+	   Display.setSpeed(10);
+    Digit first = new Five(1,9);
+    Digit second = new Nine(7,9);
+    Digit third = new Four(13,9);
+    Digit fourth = new Three(19,9);
+    Digit fifth = new Zero(25,9);
+    Digit sixth = new Zero(31,9);
+    
+    first.display();
+    second.display();
+    third.display();
+    fourth.display();
+    fifth.display();
+    sixth.display();	
+	 }	 
  }
